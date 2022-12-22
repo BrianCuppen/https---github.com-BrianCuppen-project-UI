ee51aef717407c887e0a620ab5808a9523517a0b
brian <brian.cuppen@student.howest.be>
2022-12-22 01:55:50 +0100

    commit : app.js

--- script/app.js
+++ script/app.js
@@ -1,12 +1,10 @@
 
 
-const showData = function (drinks) {
-  
-    console.log(typeof drinks);
+const showCategoryData = function (drinks) {
 
     //iterate through drinks
     for (let i = 0; i < drinks.drinks.length; i++) {
-        console.log(drinks.drinks[i].strCategory);
+        //console.log(drinks.drinks[i].strCategory);
     }
 
     //iterate through drinks and put in array
@@ -16,7 +14,7 @@ const showData = function (drinks) {
     }
     //sort the list
     drinkArray.sort();
-    console.log(drinkArray);
+    //console.log(drinkArray);
 
     //put strCategory in js-categories
     let categories = document.querySelector(".js-categories");
@@ -27,54 +25,154 @@ const showData = function (drinks) {
             //add active class
             categories.innerHTML += `<li class="c-main-nav__item is-selected">
             <a class="c-main-nav__link" active href="#">${drinkArray[i]}</a></li>`;
-        }else{
+        } else {
             //add reguarly
             categories.innerHTML += `<li class="c-main-nav__item">
             <a class="c-main-nav__link" href="#">${drinkArray[i]}</a></li>`;
         }
     }
 
-    
+
 }
 
+const showDrinkData = function (drink) {
+
+    let cocktailApp = document.querySelector(".js-Cocktails");
+    //console.log(drink);
+    //iterate through drinks
+    for (let i = 0; i < drink.drinks.length; i++) {
+         cocktailApp.innerHTML += `<div class="o-container">
+         <div class="c-dashboard">
+             <div class="c-dashboard__item u-x-span-4-bp3 u-y-span-2-bp3">
+                 <div class="c-card">
+                     <div class="c-card__body">
+                         <svg>
+                             <!-- <image  href="${drink.drinks[i].strDrinkThumb}"
+                                     width="100%" height="100%" /> -->
+                         </svg>
+                     </div>
+                 </div>
+             </div>
+             <div class="c-dashboard__item u-x-span-8-bp3">
+                 <div class="c-cad">
+                     <div class="c-card__body">
+                         <p class="c-card__body--header">${drink.drinks[i].strDrink}</p>
+                         <p>${drink.drinks[i].strCategory}</p>
+                     </div>
+                 </div>
+             </div>
+             <div class="c-dashboard__item u-x-span-4-bp3">
+                 <div class="c-card">
+                     <div class="c-card__body">
+                         <p class="c-card__body--header"> Drink type </p>
+                         <p>${drink.drinks[i].strAlcoholic}</p>
+                     </div>
+                 </div>
+             </div>
+             <div class="c-dashboard__item u-x-span-4-bp3">
+                 <div class="c-card">
+                     <div class="c-card__body">
+                         <p class="c-card__body--header"> Glass type </p>
+                         <p class="js-CocktailGlass">${drink.drinks[i].strGlass}</p>
+                     </div>
+                 </div>
+             </div>
+             <div class="c-dashboard__item u-x-span-8-bp3">
+                 <div class="c-card">
+                     <div class="c-card__body">
+                         <p class="c-card__body--header">Ingredients</p>
+                         <p class="js-CocktailIngredients">${drink.drinks[i].strIngredient1},${drink.drinks[i].strIngredient2},${drink.drinks[i].strIngredient3}</p>
+                     </div>
+                 </div>
+             </div>
+             <!-- <a class="c-link"> Add to list </p> -->
+             <div class="c-dashboard__item u-x-span-4-bp3 u-transparent">
+                 <div class="c-card__body">
+                     <button class="c-card__button o-button-reset">
+                         Add to list
+                     </button>
+                 </div>
+             </div>
+         </div>
+     </div>`;
+
+}
+}
+const getListData = function (drinks) {
+
+    //iterate through drinks
+    for (let i = 0; i < drinks.drinks.length; i++) {
+        console.log(drinks.drinks[i].strDrink + " " + drinks.drinks[i].idDrink);
+    }
+
+    //iterate through drinks and put in array
+    //make a list of all the drinks
+    let drinkArray = [];
+    for (let i = 0; i < drinks.drinks.length; i++) {
+        drinkArray.push(drinks.drinks[i].idDrink);
+    }
+    //sort the list
+    drinkArray.sort();
+    //console.log(drinkArray);
+
+        //iterate through drink info and put in array
+    for (let i = 0; i < drinkArray.length; i++) {
+        //console.log(drinkArray[i]);
+        //make url
+        let Url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + drinkArray[i];
+        fetch(Url) // Call the fetch function passing the url of the API as a parameter
+            .then((response) => response.json())
+            .then((drink) => {
+                //console.log(drink);
+                //send to show function
+                showDrinkData(drink);
+            })
+            .catch(function (err) {
+                // This is where you run code if the server returns any errors
+                console.log(err);
+            });
+
+        }
+        //send list to show function
+        //showDrinkData(drankList);
+}
 const getUrlData = function () {
     //make url
-    let Url = "http://thecocktaildb.com/api/json/v1/1/search.php?f=a"
     let Url2 = "http://thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
-   fetch(Url2) // Call the fetch function passing the url of the API as a parameter
-    .then((response) => response.json())
-    .then((drinks) => {
-        console.log(drinks);
+    fetch(Url2) // Call the fetch function passing the url of the API as a parameter
+        .then((response) => response.json())
+        .then((drinks) => {
+            console.log(drinks);
 
-        //send to show funciton
-        //showData(drinks);
-    })
-    .catch(function(err) {
-        // This is where you run code if the server returns any errors
-        console.log(err)
-    });
+            //send to show function
+            getListData(drinks);
+        })
+        .catch(function (err) {
+            // This is where you run code if the server returns any errors
+            console.log(err)
+        });
 }
 
 const getCategoryData = function () {
     //make url
     let Url = "http://thecocktaildb.com/api/json/v1/1/list.php?c=list"
     fetch(Url) // Call the fetch function passing the url of the API as a parameter
-    .then((response) => response.json())
-    .then((categories) => {
-        console.log(categories);
+        .then((response) => response.json())
+        .then((categories) => {
+            //console.log(categories);
 
-        //send to show function
-        showData(categories);
-    })
-    .catch(function(err) {
-        // This is where you run code if the server returns any errors
-        console.log(err)
-    });
+            //send to show function
+            showCategoryData(categories);
+        })
+        .catch(function (err) {
+            // This is where you run code if the server returns any errors
+            console.log(err)
+        });
 
 }
 
 // domcontentloaded event
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     console.log("DOM loaded");
     //test url
     getUrlData();
